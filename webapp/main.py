7439e9cf799e4f33a3326d840a0e8ff9781671cc
River <river@quseit.com>
2015-09-23 00:04:31 +0800

    Submmit final version

--- webapp/main.py
+++ webapp/main.py
@@ -1,5 +1,6 @@
 #-*-coding:utf-8-*-  
 #qpy:webapp:PyConChina2015
+#qpy:drawer
 #qpy://127.0.0.1:8080/
 """
 PyConChina2015 App's sourcecode
@@ -13,21 +14,41 @@ from bottle import run, debug, route, error, static_file, template, redirect
 import urllib2
 import os
 import json
+
+
 #### 常量定义 #########
 ASSETS = "/assets/"
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
 #### 安装WebApp依赖包 ####
+def _save_from_url(surl, dname):
+    jfile = ROOT+'/'+dname
+    if not os.path.exists(jfile):
+        try:
+            data = urllib2.urlopen(surl)
+            fd = open(jfile,'w')
+            content = data.read()
+            fd.write(content)
+            data.close()
+            fd.close()
+        except:
+            pass
+    else:
+        fd = open(jfile)
+        content =fd.read()
+        fd.close()
+        
 def _setup_webapp_denps():
-    pass
+    _save_from_url('http://qpython.org/libs/jsonconv.py', 'jsonconv.py')
+    _save_from_url('http://qpython.org/libs/ordereddict.py', 'ordereddict.py')
 
-try:
-    _setup_qpyapp_denps()
-except:
-    pass
+#try:
+_setup_webapp_denps()
+#except:
+#pass
 
 from jsonconv import *
+
 
 
 ######### QPYTHON WEB SERVER ###############
@@ -61,12 +82,9 @@ def __exit():
 def __ping():
     return "ok"
 
-def fonts_static(filepath):
-    return static_file(filepath, root=ROOT+'/fonts')
-
-
 def server_static(filepath):
     return static_file(filepath, root=ROOT+'/assets')
+
 
 ########################################
 PAGE_TEMP = """
@@ -121,25 +139,6 @@ font-size:18px;
 </html>
 """.replace("{{assets}}",ASSETS)
 
-######### WEBAPP ROUTERS ###############
-
-def _get_json_content():
-    jurl = 'http://cn.pycon.org/2015/pycon.json'
-    jfile = ROOT+'/pycon.json'
-    if not os.path.exists(jfile):
-        data = urllib2.urlopen(jurl)
-        fd = open(jfile,'w')
-        content = data.read()
-        fd.write(content)
-        data.close()
-        fd.close()
-    else:
-        fd = open(jfile)
-        content =fd.read()
-        fd.close()
-        
-    return content
-
 def home():
     JS = ""
     CONTENT = """
@@ -151,14 +150,15 @@ def home():
                 <span></span>                      
             </a>     
             <div style="float:right;margin: 10px 10px 0 0;" >
-                <button onclick="milib.openUrl('http://cn.pycon.org/2015/proposals.html')" class="btn btn-info" >
+                已闭幕，可下载现场记录
+                <!--button onclick="milib.openUrl('http://cn.pycon.org/2015/proposals.html')" class="btn btn-info" >
                     提交主题
-                </button>                    
+                </button-->                    
             </div>             
         </ul>
         <div style="clear:both"></div>
         <div style="padding:0px 15px 10px 15px">
-                  PyCon 是全球 Pythoneer 最盛大的年度聚会,由 PSF(Python 基金会)支持,致力于营造愉快的多元化的 Python 技术主题大会. PyConChina 是由 CPyUG(华蠎用户组)获得授权举办的 中国PyCon 年会. 迄今已是第五届, 由 PyChina.org 发起,CPyUG/TopGeek 等社区协同,在 9月13~27日 (程序员节 前后), 北京/上海/广州 联办.
+                  PyCon 是全球 Pythoneer 最盛大的年度聚会,由 PSF(Python 基金会)支持,致力于营造愉快的多元化的 Python 技术主题大会. PyConChina 是由 CPyUG(华蠎用户组)获得授权举办的 中国PyCon 年会. 迄今已是第五届, 目前大会已结束，大会幻灯，视频，录音可以通过本App下载.
         </div>
         
     </div>          
@@ -176,7 +176,7 @@ def home():
                 <a class="circle-text">
                     PyCon 北京 
                 </a>
-                <span style="color:grey;padding-left:15px;">火热报名中</span>
+                <span style="color:grey;padding-left:15px;">已结束</span>
               </span>                    
             </div><!-- /.col-lg-4 -->
           </div>
@@ -190,7 +190,7 @@ def home():
                 <a class="circle-text" >
                     PyCon 上海 
                 </a>
-                <span style="color:grey;padding-left:15px;">火热报名中</span>
+                <span style="color:grey;padding-left:15px;">已结束</span>
               </span>                    
             </div><!-- /.col-lg-4 -->
           </div>
@@ -204,7 +204,7 @@ def home():
                 <a class="circle-text" >
                     PyCon 广州 
                 </a>
-                <span style="color:grey;padding-left:15px;">火热报名中</span>
+                <span style="color:grey;padding-left:15px;">已结束</span>
 
               </span>                    
             </div><!-- /.col-lg-4 -->
@@ -219,12 +219,22 @@ def home():
     return template(PAGE_TEMP % (JS, CONTENT))
 
 
-#############
-def show_speakers():
-    content = _get_json_content()
-    jdata = json.loads(content)
-    T = u"<h4 class='tt'>演讲者</h4>%s" % json2html.convert(json=jdata['speakers'], table_attributes="class=\"table table-bordered table-hover\"")
-    return template(PAGE_TEMP % ('',T))
+def _get_json_content():
+    jurl = 'http://cn.pycon.org/2015/pycon.json'
+    jfile = ROOT+'/pycon2.json'
+    if not os.path.exists(jfile):
+        data = urllib2.urlopen(jurl)
+        fd = open(jfile,'w')
+        content = data.read()
+        fd.write(content)
+        data.close()
+        fd.close()
+    else:
+        fd = open(jfile)
+        content =fd.read()
+        fd.close()
+        
+    return content
 
 def get_speakers():
     content = _get_json_content()
@@ -261,7 +271,12 @@ $('td').each(function(){
     var val = $(this).html()
     if (typeof(data[val])!="undefined") {
         //console.log(data[val]['topic']['title'])
-        $(this).html(data[val]['topic']['title']+" - "+data[val]['name']+"<div style='color:grey'>"+data[val]['topic']['preview']+"</div>")
+        //if (data[val]['topic']['audio']!=undefined) {
+        $(this).html(data[val]['topic']['title']+" - "+data[val]['name']+"<div style='color:grey'>"+data[val]['topic']['preview']+"</div><br /><div>"+
+(data[val]['topic']['video']?"<a style=\\"font-size:20px\\"onclick=\\"milib.openUrl('"+data[val]['topic']['video']+"')\\">下载视频</a>&nbsp;&nbsp;&nbsp;":"")+
+(data[val]['topic']['audio']?"<a style=\\"font-size:20px\\"onclick=\\"milib.openUrl('"+data[val]['topic']['audio']+"')\\">下载录音</a>&nbsp;&nbsp;&nbsp;":"")+
+(data[val]['topic']['slide']?"<a style=\\"font-size:20px\\"onclick=\\"milib.openUrl('"+data[val]['topic']['slide']+"')\\">下载幻灯</a>":"")+
+"</div>")
     }
 })
 });"""
@@ -305,25 +320,13 @@ $('td').each(function(){
     for item in agd['lightening_talks']:
         M = M+json2html.convert(json=item, table_attributes="class=\"table table-bordered table-hover\"")
     M = M+u"<h5 class='tt'>已取消</h5>"
-    for item in agd['cancel_talks']:
-        M = M+json2html.convert(json=item, table_attributes="class=\"table table-bordered table-hover\"")
+
+    if agd['cancel_talks']:
+        for item in agd['cancel_talks']:
+            M = M+json2html.convert(json=item, table_attributes="class=\"table table-bordered table-hover\"")
 
 
-    D = u"""<div class="p5">
-<div class="page__disqus"><div id="disqus_thread"></div>
-<script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = 'cnpyconorg'; // required: replace example with your forum shortname
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
-</script>
-</div>
-"""
+    D = u""""""
 
 
     O = O+M
@@ -335,7 +338,6 @@ def chat():
     var connectToServer = function () {
         //Connect to your server here
         var mobileChatSocket = new SockJS('http://quseit.cn:6975/mobilechat');
-
         mobileChatSocket.onopen = function () {
             clearInterval(connectRetry);
             $('.connect-status')
@@ -343,14 +345,12 @@ def chat():
                 .addClass('connected')
                 .text('已连接');
         };
-
         //Receive message from server
         mobileChatSocket.onmessage = function (e) {
             $('#chatBox').html($('#chatBox').html() + '</br>' + e.data);
             var objDiv = document.getElementById('chatBox');
             objDiv.scrollTop = objDiv.scrollHeight;
         };
-
         mobileChatSocket.onclose = function () {
             clearInterval(connectRetry);
             connectRetry = setInterval(connectToServer, 1000);
@@ -359,7 +359,6 @@ def chat():
                 .addClass('disconnected')
                 .text('Disconnected');
         };
-
         //Send your message to the server.
         $('#sendButton').on('click', function () {
             if ($('#userName').val() != '') {
@@ -373,7 +372,6 @@ def chat():
                 objDiv.scrollTop = objDiv.scrollHeight;
             }
         });
-
         //Prevent enter refreshing the page, it sends the text from now on
         $('#messageBox').keydown(function (e) {
             if (e.keyCode == 13) { // 13 is enter
@@ -390,11 +388,9 @@ def chat():
                 return false;
             }
         });
-
         //Prevent enter refreshing the page
         $('#messageBox').keydown(function (e) {
             if (e.keyCode == 13) { // 13 is enter
-
                 return false;
             }
         });
@@ -402,7 +398,6 @@ def chat():
             $('#mynick').html($(this).val())
         });
     };
-
     var connectRetry = setInterval(connectToServer, 1000);
 """
     C="""
@@ -433,7 +428,6 @@ body {
     margin: 0px;
     margin-bottom: 1px;
 }
-
 #chatBox {
     padding:2px;
     font-family: '宋体','Arial';
@@ -452,7 +446,6 @@ body {
     margin-left: 1px;
     margin-top: 1px;
 }
-
 .disconnected {
     color: red;
 }
@@ -463,20 +456,16 @@ body {
     font-size:13px
 }
 </style>
-
     <div class="container">
     <h4>PyConChina 聊天室 <span style='padding-top:5px' class="status">( 状态:<span class="connect-status disconnected">断线</span> )</span> <span style='font-size:13px'><a data-toggle="modal" data-target="#myModal" href='#' id='mynick' style='float:right'>设置昵称</a></span></h4>
     <hr />
     <span style='color:grey'>无认证，不存历史纪录，不支持刷新。<br/>想看看谁在？吼一嗓子。</span>
     <div id="chatBox"></div>
     
-
     </div>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/sockjs-client/0.3.4/sockjs.min.js"></script>
-
     <footer class="footer">
       <div class="container" style='padding-top:10px'>
-
         
         <form id="messageForm" class="form-group">
             <input id="messageBox" type="text" value="" class='form-control' placeholder='回车发言'>
@@ -484,7 +473,6 @@ body {
         </form>
       </div>
     </footer>
-
 <!-- 模态框（Modal） -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" 
    aria-labelledby="myModalLabel" aria-hidden="true">
@@ -494,7 +482,7 @@ body {
          <div class="modal-header">
             <button type="button" class="close" 
                data-dismiss="modal" aria-hidden="true">
-                  &times;
+                  ×
             </button>
             <h4 class="modal-title" id="myModalLabel">
                设置昵称
@@ -511,28 +499,23 @@ body {
       </div><!-- /.modal-content -->
   </form>
 </div><!-- /.modal -->
-
-
 """
     return template(PAGE_TEMP % (J,C))
 
-def work():
-    redirect("https://qwork.quseit.cn")
 
 ######### WEBAPP ROUTERS ###############
 app = Bottle()
 app.route('/', method='GET')(home)
-app.route('/chat', method='GET')(chat)
-app.route('/work', method='GET')(work)
-app.route('/speakers/', method='GET')(get_speakers)
-app.route('/speakers/show/', method='GET')(show_speakers)
+app.route('/__exit', method=['GET','HEAD'])(__exit)
+app.route('/__ping', method=['GET','HEAD'])(__ping)
+app.route('/assets/:filepath', method='GET')(server_static)
+
 app.route('/beijing/agenda', method='GET')(beijing)
 app.route('/shanghai/agenda', method='GET')(shanghai)
 app.route('/guangzhou/agenda', method='GET')(guangzhou)
-app.route('/__exit', method=['GET','HEAD'])(__exit)
-app.route('/__ping', method=['GET','HEAD'])(__ping)
-app.route('/assets/<filepath:path>', method='GET')(server_static)
-app.route('/fonts/<filepath:path>', method='GET')(fonts_static)
+app.route('/speakers/', method='GET')(get_speakers)
+app.route('/chat', method='GET')(chat)
+
 
 try:
     server = MyWSGIRefServer(host="127.0.0.1", port="8080")
